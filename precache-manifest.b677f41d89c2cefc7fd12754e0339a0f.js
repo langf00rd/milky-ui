00362feb2813afb1210bdf14bbb18f9216e47947
Langford <langfordquarshie21@gmail.com>
2022-08-08 15:27:34 +0000

    Updates

---
+++ precache-manifest.b677f41d89c2cefc7fd12754e0339a0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0acc7ef779f52b97505826ca080578b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "84b7636b9c60dd49395e",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "9a8887daef3fb4fe364e",
+    "url": "./static/js/2.fed91245.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.fed91245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b7636b9c60dd49395e",
+    "url": "./static/js/main.7f1af12b.chunk.js"
+  },
+  {
+    "revision": "bde3262ae25e283dc0fb",
+    "url": "./static/js/runtime-main.67065e73.js"
+  }
+]);
